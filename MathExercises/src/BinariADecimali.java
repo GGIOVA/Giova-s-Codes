a2f76c2bc72e22ac78650218c4be21ee7d527535
itsadmin <itsadmin@192.168.0.138>
2017-11-28 16:50:25 +0100

    binariADecimali

---
+++ MathExercises/src/BinariADecimali.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class BinariADecimali {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("");
+		int[] numeri = new int[10];
+		int cont=0;
+		
+			for(int i=0; i<10; i++) {
+				
+				System.out.println("Inserisci al massimo 10 elementi tra 0 o 1, per uscire digita -1: ");
+				
+				cont = sc.nextInt();
+				
+				
+				if(cont==-1) {
+					for(int j=i;j<10;j++) {
+						numeri[j]=0;
+					} 
+					break;
+				}
+				
+				
+			}
+				long result=0;
+					for(int x=0;x < numeri.length;x++) {
+						int potenzaDiDue=1;
+						for(int j =0;j<x;j++) {
+							potenzaDiDue=potenzaDiDue*2;
+						}
+	result=result+(numeri[x]*potenzaDiDue);
+						
+					}
+				System.out.println("Il numero in base dieci è: " + result);
+				
+			
+		
+	}
+
+}  // if(numeri[i]!=0 ||numeri[i]!=1 || numeri[i]!=-1) {
+         //System.out.println("errore");
